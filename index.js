7480bd36f3b51723ba25656640746efa33d6516e
DevGangMin <fire0609@naver.com>
2025-09-27 08:41:53 +0900

    Chore: add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+require('dotenv').config();
+
 const connectDB = require('./src/config/db');
 const userRoutes = require('./src/api/users/user.routes');
 
 const app = express();
 const port = 3001;
+
+app.use(express.json());
 
 connectDB();
 
