b4ba1ebdad1298795cc30ef90292790ef5e1834f
DevGangMin <fire0609@naver.com>
2025-09-27 07:27:19 +0900

    connect db

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const connectDB = require('./src/config/db');
 const userRoutes = require('./src/api/users/user.routes');
 
 const app = express();
 const port = 3001;
+
+connectDB();
 
 app.use('/api/users', userRoutes);
 
